3f6159bc712270b0db6bc4caf5eb0e782096eb91
Izunnajune <151368567+Izunnajune@users.noreply.github.com>
2023-11-22 18:01:35 +0100

    Delete FiveDgts.py

--- FiveDgts.py
+++
@@ -1,9 +0,0 @@
-FiveDgts = int(input('kindly enter five digits:'))
- 
-dgt5 = (FiveDgts%10)
-dgt4 = (FiveDgts%100)
-dgt3 = (FiveDgts%1000)
-dgt2 = (FiveDgts%10000)
-dgt1 = (FiveDgts%100000)
-
-print(dgt1)
