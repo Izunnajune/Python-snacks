60e6b31479b7b9b0a7093e4ca4c7db67d74b5e2b
Izunnajune <151368567+Izunnajune@users.noreply.github.com>
2023-11-23 01:20:57 +0100

    Add files via upload

---
+++ FiveDgts.py
@@ -0,0 +1,21 @@
+FiveDgts = int(input('kindly enter five digits:'))
+ 
+dgt1 = (FiveDgts%10)
+
+dgt2 = (FiveDgts//10)
+
+dgt3 = (dgt2%10)
+
+dgt4 = (dgt2//10)
+
+dgt5 = (dgt4%10)
+
+dgt6 = (dgt4//10)
+
+dgt7 = (dgt6%10)
+
+dgt8 = (dgt6//10)
+
+
+print(dgt8,   dgt7,   dgt5,   dgt3,   dgt1)
+
